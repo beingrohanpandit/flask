9a7aa2a2dcf169e3d8cab2814a27c4356791b03b
Rohan Sharma <rohansharma12434@gmail.com>
2023-04-27 13:57:06 +0530

    Connect The DataBase and Insert The Data into

--- hello.py
+++ hello.py
@@ -2,11 +2,45 @@ from flask import Flask, render_template,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 #create a Flask Instance
 
 app = Flask(__name__)
+
+# Add DataBase
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/user'
+
+
 app.config['SECRET_KEY'] = "1245"
+
+# Initialize the DataBase
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100),nullable=True, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # Create a String
+    def __repr__(self):
+        return'<Name %r>' % self.name
+
+# Create all tables in the database
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+create_tables()
 # Create a Form Class
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 class NameForm(FlaskForm):
     name = StringField("What is Your Name", validators=[DataRequired()])
@@ -50,3 +84,24 @@ def name():
     return render_template("name.html",
     name=name,
     form=form)
+
+# Create a From Page
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first() 
+        if user is None:
+            user = Users(name=form.name.data,email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("Data Submitted Successfully")
+    user = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html',
+    form=form,
+    name=name,
+    user=user)
